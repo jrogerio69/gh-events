8909ccaa9e76530194b517829b52f25089438796
jrogerio69 <jrogerio69@gmail.com>
2024-07-02 13:40:12 -0300

    Changed the global user.name and user.email to jrogerio69

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -7,6 +7,7 @@ import MainContent from "./MainContent";
 // My tests
 // Add yet another comment
 // To see if the user jrogerio69 is doing the commit command
+// Change the global user.name and user.email to jrogerio69
 
 describe("MainContent", () => {
   it("should render a button", () => {
